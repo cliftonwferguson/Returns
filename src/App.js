7afa12ad9bbedd5c27d5e7dba3efbcd02df40647
Clifton Ferguson <clifton.ferguson7@gmail.com>
2020-04-19 18:34:32 -0500

    Plugged State

--- src/App.js
+++ src/App.js
@@ -10,11 +10,24 @@ class Table extends Component {
       super(props) 
       this.state = { 
          indexReturns: [
-            { year: '2016', yields: 11.96, cumulative: 1 },
-            { year: '2017', yields: 21.83, cumulative: 1 },
-            { year: '2018', yields: -4.38, cumulative: 2 },
-            { year: '2019', yields: 31.49, cumulative: 3},
-            { year: '2020', yields: -13.34,cumulative: 4},
+            { year: '2002', yields: -22.1, cumulative: 0.86 },
+            { year: '2003', yields: 28.68, cumulative: -2.30 },
+            { year: '2004', yields: 10.88, cumulative: -0.62 },
+            { year: '2005', yields: 4.91, cumulative: -0.55 },
+            { year: '2006', yields: 15.79, cumulative: 2.22 },
+            { year: '2007', yields: 5.49, cumulative: -0.65 },
+            { year: '2008', yields: -37, cumulative: -7.74 },
+            { year: '2009', yields: 26.46, cumulative: -1.72 },
+            { year: '2010', yields: 15.06, cumulative: -0.43 },
+            { year: '2011', yields: 2.11, cumulative: -0.86 },
+            { year: '2012', yields: 16.00, cumulative: 6.58 },
+            { year: '2013', yields: 32.39, cumulative: 1.02 },
+            { year: '2014', yields: 13.69, cumulative: -0.58 },
+            { year: '2015', yields: 1.38, cumulative: -0.90 },
+            { year: '2016', yields: 11.96, cumulative: 7.67 },
+            { year: '2017', yields: 21.83, cumulative: .83 },
+            { year: '2018', yields: -4.38, cumulative: -1.20 },
+            { year: '2019', yields: 31.49, cumulative: -8.19},
          ]
       }
    }
